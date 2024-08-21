6fdccfc395065757a19c1c213d8f9c85012f9fe0
adeliamara <adeliamara13@gmail.com>
2024-08-21 11:14:51 -0300

    feat: modify method

--- javascript/sorted.js
+++ javascript/sorted.js
@@ -1,26 +1,30 @@
-function getSortedEvenNumbers(numbers) {
-    let evenNumbers = [];
 
-    // Filtra números pares
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] % 2 === 0) {
-            evenNumbers.push(numbers[i]);
-        }
-    }
-
-    // Ordena números pares
-    for (let i = 0; i < evenNumbers.length; i++) {
-        for (let j = i + 1; j < evenNumbers.length; j++) {
-            if (evenNumbers[i] > evenNumbers[j]) {
-                let temp = evenNumbers[i];
-                evenNumbers[i] = evenNumbers[j];
-                evenNumbers[j] = temp;
-            }
-        }
-    }
-
-    return evenNumbers;
+function getSortedEvenNumbers(numbers){
+    return numbers
 }
+// function getSortedEvenNumbers(numbers) {
+//     let evenNumbers = [];
+
+//     // Filtra números pares
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (numbers[i] % 2 === 0) {
+//             evenNumbers.push(numbers[i]);
+//         }
+//     }
+
+//     // Ordena números pares
+//     for (let i = 0; i < evenNumbers.length; i++) {
+//         for (let j = i + 1; j < evenNumbers.length; j++) {
+//             if (evenNumbers[i] > evenNumbers[j]) {
+//                 let temp = evenNumbers[i];
+//                 evenNumbers[i] = evenNumbers[j];
+//                 evenNumbers[j] = temp;
+//             }
+//         }
+//     }
+
+//     return evenNumbers;
+// }
 
 
 /*
